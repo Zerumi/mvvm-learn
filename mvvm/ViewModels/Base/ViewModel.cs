c88a6637d9bfc23fb78499c1d5da5554a4e8e1d2
Zerumi <evgexa.2015@bk.ru>
2022-08-09 02:28:17 +0400

    IDisposable implementation

--- mvvm/ViewModels/Base/ViewModel.cs
+++ mvvm/ViewModels/Base/ViewModel.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace mvvm.ViewModels.Base
 {
-    internal abstract class ViewModel : INotifyPropertyChanged
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
@@ -23,6 +23,22 @@ namespace mvvm.ViewModels.Base
             field = value;
             OnPropertyChanged(PropertyName);
             return true;
+        }
+
+        ~ViewModel() => Dispose(false);
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private bool _Disposed;
+
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
+            // Освобождение управляемых ресурсов
         }
     }
 }
